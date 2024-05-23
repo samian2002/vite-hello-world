8c0c41fc88157dfcbf8aea9db2c8efa0b8246338
samian2002 <samiantariq14@gmail.com>
2024-05-23 12:26:08 +0200

    bonus 2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h1>Hello, World!</h1>
+    <HelloWorld />
   </div>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
